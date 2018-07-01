cab174a7d319d0de3b025bc893ab2df700a42cd2
Alcieres <alcieres@gmail.com>
2018-07-01 15:34:10 -0300

    Alterada classe principal de Main

--- src/main/java/com/example/employees/Main.java
+++ src/main/java/com/example/employees/Main.java
@@ -1,6 +1,7 @@
 /* Copyright 2015 Oracle and/or its affiliates. All rights reserved. */
 package com.example.employees;
 
+import java.util.Optional;
 import org.apache.catalina.startup.Tomcat;
 //Import the Optional class
 
@@ -13,7 +14,7 @@ public class Main {
         String contextPath = "/" ;
         String appBase = ".";
         Tomcat tomcat = new Tomcat();  
-        //Use this PORT variable to set the port on Tomcat instance     
+        tomcat.setPort(Integer.valueOf(PORT.orElse("8081")));     
         tomcat.getHost().setAppBase(appBase);
         tomcat.addWebapp(contextPath, appBase);
         tomcat.start();
