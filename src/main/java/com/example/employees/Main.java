da4b4d88facda759e4eef9d008a0938555b6f85a
Alcieres <alcieres@gmail.com>
2018-07-02 20:39:57 -0300

    Classes atualizadas

--- src/main/java/com/example/employees/Main.java
+++ src/main/java/com/example/employees/Main.java
@@ -1,20 +1,21 @@
-/* Copyright 2015 Oracle and/or its affiliates. All rights reserved. */
+/* Copyright © 2015 Oracle and/or its affiliates. All rights reserved. */
 package com.example.employees;
 
 import java.util.Optional;
+
 import org.apache.catalina.startup.Tomcat;
-//Import the Optional class
 
 public class Main {
     
     public static final Optional<String> PORT = Optional.ofNullable(System.getenv("PORT"));
-    //Create a PORT instance variable
+    public static final Optional<String> HOSTNAME = Optional.ofNullable(System.getenv("HOSTNAME"));
     
     public static void main(String[] args) throws Exception {
         String contextPath = "/" ;
         String appBase = ".";
-        Tomcat tomcat = new Tomcat();  
-        tomcat.setPort(Integer.valueOf(PORT.orElse("8081")));     
+        Tomcat tomcat = new Tomcat();   
+        tomcat.setPort(Integer.valueOf(PORT.orElse("8080") ));
+        tomcat.setHostname(HOSTNAME.orElse("localhost"));
         tomcat.getHost().setAppBase(appBase);
         tomcat.addWebapp(contextPath, appBase);
         tomcat.start();
